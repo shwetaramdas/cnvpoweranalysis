2a70c2d7b2205cbc3743d6190178292aee3e22c7
Shweta <shwetaramdas@users.noreply.github.com>
2018-06-18 11:25:33 -0400

    Update app.R

--- shiny/app.R
+++ shiny/app.R
@@ -1,7 +1,10 @@
 library(shiny)
 library(markdown)
-source("functions.R")
+library(ggplot2)
+library(grid)
 
+source("functions.R")
+source("functions_plot.R")
 
 # Define UI for CNV calculator ----
 ui <- shinyUI(pageWithSidebar(
@@ -14,16 +17,14 @@ ui <- shinyUI(pageWithSidebar(
   sidebarPanel(
     helpText("Enter experimental parameters to calculate power"),
     radioButtons('dis', label = 'Distribution*', c('Poisson' = 'pois','Negative Binomial' = 'nb')),
-    numericInput("F", "Tumor Purity*:", 1,min=0,max=1),
-    numericInput("alpha", "Significance Level alpha*:", 0.05,min=0,max=1),
-    numericInput("L", "Length of CNV*:", 10000,min=0),
+    numericInput("F", "Tumor Purity (range 0-1)*:", 1,min=0,max=1),
+    numericInput("alpha", "Significance Level alpha (range 0-1)*:", 0.05,min=0,max=1),
+    numericInput("L", "Length of CNV (in bases)*:", 10000,min=0),
     numericInput("N", "Ploidy of CNV*:", 3,min=0),
-    numericInput("W", "Window length*:", 100,min=0),
-    numericInput("l", "Length of read*:", 100,min=0),
-    numericInput("D", "Read Depth*: ", 30,min=0),
-    numericInput("phi","Overdispersion parameter phi (optional):",min=0,max=1,value = ''),
-    helpText("To generate a power curve, select experimental parameter to vary"),
-    selectInput('tovary',label="Parameter to Vary", choices=c('None','Read Depth','Read length','Window size','Length of CNV'))
+    numericInput("W", "Window length (in bases)*:", 100,min=0),
+    numericInput("l", "Length of read (bases)*:", 100,min=0),
+    numericInput("D", "Haploid Read Depth (integer value)*: ", 30,min=0),
+    numericInput("phi","Overdispersion parameter mu*phi (optional):",min=0,max=1,value = '')
   ),
   
   # Show a summary of the dataset and an HTML table with the requested
@@ -32,7 +33,18 @@ ui <- shinyUI(pageWithSidebar(
     tabsetPanel(type = "tabs",
                 tabPanel("Input Parameters", verbatimTextOutput("toPrint")),
                 tabPanel("Power", verbatimTextOutput("power")),
-                tabPanel("Power Curve",verbatimTextOutput("powerCurve_text"), plotOutput("powerCurve")),
+                tabPanel("Power Curve",verbatimTextOutput("powerCurve_text"),
+                         selectInput('tovary',label="First parameter to vary along x-axis", choices=c('None','Read Depth','Read length','Window size','Length of CNV','Sample purity')),
+                         numericInput("min1","Minimum value of parameter to be tested",1),
+                         numericInput("max1","Maximum value of parameter to be tested",10),
+
+                         selectInput('tovary2',label="Second Parameter to Vary (line color)", choices=c('None','Read Depth','Read length','Window size','Length of CNV','Sample purity')),
+                         numericInput("val1","Value 1 to be tested for parameter 2",1),
+                         numericInput("val2","Value 2 to be tested for parameter 2",2),
+                         numericInput("val3","Value 3 to be tested for parameter 2",3),
+                         plotOutput("powerCurve",width="75%"),
+                         downloadButton("downloadData", "Download Raw Values")
+                ),
                 tabPanel("About",includeMarkdown("README.md"))
                 
     )
@@ -53,17 +65,17 @@ server = shinyServer(function(input, output) {
   })
     
   output$toPrint <- renderText({
-      sprintf(" L: %s \n D: %s \n W: %s \n N: %s \n l: %s \n" ,input$L, input$D, input$W, input$N, input$l)
+      sprintf(" L: %s \n D: %s \n W: %s \n N: %s \n l: %s \n F: %s \n" ,input$L, input$D, input$W, input$N, input$l, input$F)
   })
   # Show the first "n" observations
   output$view <- renderTable({
 
   })
-  
+ 
   output$power <- renderText({
     if(input$dis =="pois"){
       if(input$F == 1){
-        paste("Power is", poisson_pure(as.numeric(input$alpha), as.numeric(input$W), as.numeric(input$l), as.numeric(input$D),as.numeric(input$N), as.numeric(input$L)))
+        paste("Power is", round(poisson_pure(as.numeric(input$alpha), as.numeric(input$W), as.numeric(input$l), as.numeric(input$D),as.numeric(input$N), as.numeric(input$L)),3))
       }else{
         paste("Power is", poisson_impurity(as.numeric(input$alpha), as.numeric(input$W), as.numeric(input$l), as.numeric(input$D),as.numeric(input$N), as.numeric(input$L),input$F))
       }  
@@ -79,52 +91,24 @@ server = shinyServer(function(input, output) {
   })
   ##########################
   ##Generate a power curve##
-  output$powerCurve <- renderPlot({
-    ps = c()
-    if(input$tovary == 'None'){
-      print('Select a parameter to vary to generate a power curve.')
-    }
-    else if(input$tovary=='Read Depth'){
-      Ds = c(0.1,1,10,20,30,50,60,100)
-      for(d in c(0.1,1,10,20,30,50,60,100)){
-        if(input$dis == "pois"){
-         if(input$F == 1){ 
-          ps = c(ps,poisson_pure(as.numeric(input$alpha), as.numeric(input$W), as.numeric(input$l), d,as.numeric(input$N), as.numeric(input$L)))
-         }else{
-           ps = c(ps,poisson_impurity(as.numeric(input$alpha), as.numeric(input$W), as.numeric(input$l), d,as.numeric(input$N), as.numeric(input$L),input$F)) 
-         }
-        }else{
-          if(is.na(input$phi)){phi = 1/(input$W*input$D/input$l);}
-          else{phi = input$phi;}
-          if(input$F == 1){
-            ps = c(ps,negativebinomial_purity(input$alpha,input$W,input$l,input$D,input$N,input$L,phi,input$F))
-          }else{
-            ps = c(ps, negativebinomial_purity(input$alpha,input$W,input$l,input$D,input$N,input$L,phi,input$F))
-          }
-        }  
-      }
-      plot(Ds, ps,type="l",xlab="Read Depth",ylab="Power")
-      points(Ds, ps,pch=19);
-    }
-    else if(input$tovary == 'Read length'){
-      ls = c(50,75,100,150)
-      for(l in ls){
-        ps = c(ps,poisson_pure(as.numeric(input$alpha), as.numeric(input$W), l, as.numeric(input$D),as.numeric(input$N), as.numeric(input$L)))
-      }
-      plot(ls, ps,type="l",xlab="Read Length",ylab="Power")
-      points(ls, ps,pch=19);
-      
-    }
-
-  })
+  output$powerCurve <- renderPlot({plot_power(input)});
   output$powerCurve_text <- renderText({
     if(input$tovary == 'None'){
       sprintf("Select parameter to vary to plot a power curve")
     }
     else{
-      donothing = 1;
+      sprintf(" L: %s \n D: %s \n W: %s \n N: %s \n l: %s \n" ,input$L, input$D, input$W, input$N, input$l)
     }
-  })  
+  })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(powervalues(input),file, row.names = FALSE)
+    }
+  )
 })
 
 shinyApp(ui, server)
