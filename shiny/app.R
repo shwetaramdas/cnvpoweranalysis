83744ef8a575a5c5eeb4d66ae7f1df59e3d7c332
HanyouITW <40808480+HanyouITW@users.noreply.github.com>
2018-07-16 14:29:52 -0400

    Update app.R

--- shiny/app.R
+++ shiny/app.R
@@ -6,7 +6,6 @@ library(grid)
 
 # Source the functions from the coresponding files
 source("functions.R")
-source("functions_plot.R")
 
 # Define UI for CNV calculator ----
 ui <- pageWithSidebar(
@@ -28,8 +27,7 @@ ui <- pageWithSidebar(
     numericInput("phi","Overdispersion parameter mu*phi or theta (0 for Poisson Distribution):", min=0, value = 0)
   ),
   
-  # Main Panel to display a summary of inputs, the calculated power, and a panel for
-  # users to generate their own power curve
+  # Main Panel to display a summary of inputs, the calculated power, and a panel for users to generate their own power curve
   mainPanel(
     tabsetPanel(type = "tabs",
                 # To print a summary of the input parameters and the calculated power
@@ -38,7 +36,7 @@ ui <- pageWithSidebar(
                 tabPanel("Power Curve",verbatimTextOutput("powerCurve_text"),
                          selectInput('tovary',label="First parameter to vary along x-axis", choices=c('None','Read Depth','Read length','Window size','Length of CNV','Sample purity')),
                          numericInput("min1","Minimum value of parameter to be tested",1),
-                         numericInput("max1","Maximum value of parameter to be tested",10),
+                         numericInput("max1","Maximum value of parameter to be tested",10000),
 
                          selectInput('tovary2',label="Second Parameter to Vary (line color)", choices=c('None','Read Depth','Read length','Window size','Length of CNV','Sample purity')),
                          numericInput("val1","Value 1 to be tested for parameter 2",1),
@@ -61,31 +59,30 @@ server = function(input, output) {
   # To print a summary of input parameters and the calcualted power
   output$toPrint <- renderText({
     # To calcualte the power coresponding to the inputs
-    calculated_power <- UA_CPA (as.numeric(input$alpha), as.numeric(input$W), as.numeric(input$l),
-                                as.numeric(input$D), as.numeric(input$N), as.numeric(input$L),
-                                as.numeric(input$phi), as.numeric(input$F))
+    calculated_power <- UA_CPA (as.numeric(input$alpha), as.numeric(input$W), as.numeric(input$l), as.numeric(input$D), as.numeric(input$N), as.numeric(input$L), as.numeric(input$phi), as.numeric(input$F))
     # To print out a list of the input parameters and the calculated power
     paste(sprintf("Distribution: %s\nTumor Purity, F: %s\nSignificance Level, alpha: %s\nLength of CNV, L: %s\nPloidy of CNV, N: %s\nWindow length, W: %s\nLength of read, l: %s\nHaploid Read Depth, D: %s\nOverdispersion parameter, theta: %s\n\n",
               input$dis, input$F, input$alpha, input$L, input$N, input$W, input$l, input$D, input$phi), paste("\nThe power for this experiment design is", format(round(calculated_power, 2), nsmall = 2)))
   })
 
   # Generate a power curve
-  output$powerCurve <- renderPlot({plot_power(input)});
+  output$powerCurve <- renderPlot({UA_CPAP(input)});
   output$powerCurve_text <- renderText({
     if(input$tovary == 'None'){
       sprintf("Select parameter to vary to plot a power curve")
     }
     else{
-      sprintf(" L: %s \n D: %s \n W: %s \n N: %s \n l: %s \n" ,input$L, input$D, input$W, input$N, input$l)
+      sprintf("Distribution: %s\nTumor Purity, F: %s\nSignificance Level, alpha: %s\nLength of CNV, L: %s\nPloidy of CNV, N: %s\nWindow length, W: %s\nLength of read, l: %s\nHaploid Read Depth, D: %s\nOverdispersion parameter, theta: %s\n\n",
+              input$dis, input$F, input$alpha, input$L, input$N, input$W, input$l, input$D, input$phi)
     }
   })
   
+  # Allow the user to download the data
   output$downloadData <- downloadHandler(
-    filename = function() {
-      paste(input$dataset, ".csv", sep = "")
-    },
+    filename = "raw_data.csv",
     content = function(file) {
-      write.csv(powervalues(input),file, row.names = FALSE)
+      write.csv(
+        x = df_1, file) # df_1 is a global variable of data frame that is generated in the function UA_CPAP
     }
   )
 }
